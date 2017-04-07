99ca945d235a1a41e6e352f5b2f8472a8faeedf5
AngleNet <anglenet@sina.cn>
2017-04-07 22:52:35 +0800

    Update

--- Runner/userWeiboSpider.py
+++ Runner/userWeiboSpider.py
@@ -4,6 +4,7 @@ import Spider.userWeiboSpider
 import Spider.utils
 if __name__ == '__main__':
     data_dir = sys.argv[1]
+    Spider.utils.loadSUB(data_dir+'.sub')
     with codecs.open('user_links.new', 'r', 'utf-8') as fd:
         for line in fd.readlines():
             user = Spider.utils.userLineSpliter(line)
