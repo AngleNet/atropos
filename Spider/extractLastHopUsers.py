5aa8907aa9513b92cd35da56d9e2157bc9f9454e
AngleNet <anglenet@sina.cn>
2017-04-09 18:02:18 +0800

    Update

--- Spider/extractLastHopUsers.py
+++ Spider/extractLastHopUsers.py
@@ -2,8 +2,8 @@ import os, codecs
 import Spider.utils
 
 def extractUsers(uid, links, ouids, data_dir):
-    tweet_fname = uid + '.tweet'
-    origin_tweet_fname = uid + '.origin_tweet'
+    tweet_fname = data_dir + uid + '.tweet'
+    origin_tweet_fname = data_dir + uid + '.origin_tweet'
     if not os.path.exists(data_dir+tweet_fname) or \
             not os.path.exists(data_dir+origin_tweet_fname):
         Spider.utils.debug('Missing .tweet or .origin_tweet file of ' + uid)
