229f8fc624d73bdd7bb9c142edb5fb8aeef4cdf2
AngleNet <anglenet@sina.cn>
2017-05-03 23:04:05 +0800

    update

--- Runner/spideOriginalWeibo.py
+++ Runner/spideOriginalWeibo.py
@@ -10,17 +10,17 @@ import Spider.userWeiboSpider
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        proj_dir = '../'
+        proj_dir = '..'
     else:
         proj_dir = sys.argv[1]
-    res_dir = proj_dir + 'result/'
-    data_dir = proj_dir + 'data/'
-    Spider.utils.loadSUB(data_dir+'.sub')
-    ouser_link_fname = 'user_links.original.new'
-    user_links = Spider.utils.loadOriginalUserLinks(data_dir+ ouser_link_fname)
-    for user_link in user_links.values():
-        user = Spider.utils.userLinkToUser(user_link)
-        if user.id == '' or user.link == '' or user.page_id == '':
-            Spider.utils.debug('required fields are empty in {user}'.format(user=str(user)))
-            continue
-        Spider.userWeiboSpider.spideUser(user, data_dir, spide_original=True)
+    data_dir = proj_dir + '/data'
+    res_dir = proj_dir + '/result'
+    Spider.utils.loadSUB(data_dir + '/.sub')
+    with codecs.open(data_dir + '/user_links.new', 'r', 'utf-8') as fd:
+        for line in fd.readlines():
+            user = Spider.utils.userLineSpliter(line)
+            if user.id == '' or user.link == '' or user.page_id == '':
+                Spider.utils.debug('required fields are empty in {user}'.format(user=str(user)))
+                continue
+            Spider.userWeiboSpider.spideUser(user, data_dir, spide_original=True)
+
--- Runner/userWeiboSpider.py
+++ Runner/userWeiboSpider.py
@@ -5,9 +5,14 @@ import Spider.userWeiboSpider
 
 import Spider.utils
 if __name__ == '__main__':
-    data_dir = sys.argv[1]
-    Spider.utils.loadSUB(data_dir+'.sub')
-    with codecs.open(data_dir + 'user_links.new', 'r', 'utf-8') as fd:
+    if len(sys.argv) < 2:
+        proj_dir = '..'
+    else:
+        proj_dir = sys.argv[1]
+    data_dir = proj_dir + '/data'
+    res_dir = proj_dir + '/result'
+    Spider.utils.loadSUB(data_dir+'/.sub')
+    with codecs.open(data_dir + '/user_links.new', 'r', 'utf-8') as fd:
         for line in fd.readlines():
             user = Spider.utils.userLineSpliter(line)
             if user:
