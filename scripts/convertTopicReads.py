21277924b58f16ea586047a849ce7c22054636c4
AngleNet <anglenet@sina.cn>
2017-05-30 22:24:37 +0800

    Update

--- scripts/convertTopicReads.py
+++ scripts/convertTopicReads.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
             topic.idx = idx
             topic.name = name
             topic.reads = reads
-            topics[_day][_time] = topic
+            topics[_day][_time].append(topic)
     for _day in sorted(topics.keys()):
         for _time in sorted(topics[_day].keys()):
             with codecs.open('{dir}/{ts}.topic'.format(dir=res_dir, ts=_day[:4] + '-' + _day[4:6] + '-' + _day[6:8] + _time + '0000'), 'w', 'utf-8') as fd:
